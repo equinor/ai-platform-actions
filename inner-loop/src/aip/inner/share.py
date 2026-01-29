7563b9b63e033f1e0248458c5c66ad67288daafd
Floris Elzinga <felz@equinor.com>
2026-01-29 15:54:18 +0100

    cast version to int explicitly

--- inner-loop/src/aip/inner/share.py
+++ inner-loop/src/aip/inner/share.py
@@ -193,7 +193,7 @@ def environment(
         req_int_version=True
     )
     # find latest registry version to use
-    latest_reg_version=ws_env.version # Can cause error if set=0 like the other asset types.
+    latest_reg_version = int(ws_env.version) # Can cause error if set=0 like the other asset types.
     if list_env_reg:
         for e in list_env_reg:
             lrv = int(e.version)
